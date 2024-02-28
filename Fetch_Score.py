92b4c81f8a245bad92bedf40493f4dc297915b34
Surya Abhinai <124990558+Surya-Abhinai@users.noreply.github.com>
2024-02-29 00:42:18 +0530

    Update Fetch_Score.py

--- Fetch_Score.py
+++ Fetch_Score.py
@@ -1,12 +1,11 @@
-from qdrant_client import QdrantClient
-from sentence_transformers import SentenceTransformer
-
-
 class Fetch:
-    def __init__(self, text):
-        self.client = QdrantClient(path="Qdrant_db")
-        self.encoder = SentenceTransformer("all-MiniLM-L6-v2")
+    def __init__(self,text,client,encoder):
+        self.client = client
+        self.encoder = encoder
         self.query_embedding = self.encoder.encode(text)
         self.hits = self.client.search(collection_name="Research_papers", query_vector=self.query_embedding,limit=1)
         self.score = self.hits[0].score
         self.number = self.hits[0].payload["number"]
+
+
+#1)we have to do overall score
